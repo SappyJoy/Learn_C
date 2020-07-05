002c3be6d74953d08ac0598cc13c6179f1159213
Sappy-Joy <stponomarevw@gmail.com>
2020-07-05 22:05:38 +0500

    4 глава

---
+++ The_C_Programming_language_2e/chapter_4/polska_calculator/getch_ungetch.c
@@ -0,0 +1,21 @@
+//
+// Created by Sappy-Joy on 7/5/2020.
+//
+#include <stdio.h>
+#define BUFSIZE 100
+
+char buf[BUFSIZE];      /* буфер для ungetch */
+int bufp = 0;           /* следующая свободная позиция в buf */
+
+/* ввод символа (возможно возвращаемого в поток) */
+int getch(void) {
+    return (bufp > 0) ? buf[--bufp] : getchar();
+}
+
+/* возвращение символа в поток ввода */
+void ungetch(int c) {
+    if (bufp >= BUFSIZE)
+        printf("ungetch: too many characters\n");
+    else
+        buf[bufp++] = c;
+}
